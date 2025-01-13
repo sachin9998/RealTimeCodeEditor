319b9a00e36d07c86c276fc2b94aeec0ac02e567
Sachin Alam <sachinalam4216@gmail.com>
2025-01-13 10:44:36 +0530

    Join room added

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,35 @@ const io = new Server(server, {
   },
 });
 
+const rooms = new Map();
+
 io.on("connection", (socket) => {
   console.log("User Conncted :", socket.id);
+
+  let currentRoom = null;
+  let currentUser = null;
+
+  socket.on("join", ({ roomId, userName }) => {
+    if (currentRoom) {
+      socket.leave(currentRoom);
+      rooms.get(currentRoom).delete(currentUser);
+      io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+    }
+
+    currentRoom = roomId;
+    currentUser = userName;
+
+    socket.join(roomId);
+
+    if (!rooms.has(roomId)) {
+      rooms.set(roomId, new Set());
+    }
+
+    rooms.get(roomId).add(userName);
+
+    io.to(roomId).emit("userJoined", Array.from(rooms.get(currentRoom)));
+    console.log("User joined room ", roomId);
+  });
 });
 
 server.listen(port, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,42 @@ const socket = io("http://localhost:4000");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
+  const [roomId, setRoomId] = useState("");
+  const [username, setUserName] = useState("");
+
+  const joinRoom = () => {
+    // console.log(roomId, username);
+
+    if (roomId && username) {
+      socket.emit("join", { roomId, username });
+      setJoined(true);
+    }
+  };
 
   if (!joined) {
-    return <div>App not Joined</div>;
+    return (
+      <div className="join-container">
+        <div className="join-form">
+          <h1>Join Code Room</h1>
+
+          <input
+            type="text"
+            placeholder="Room ID"
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
+          />
+
+          <input
+            type="text"
+            placeholder="Your Name"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+
+          <button onClick={joinRoom}>Join Room</button>
+        </div>
+      </div>
+    );
   }
 
   return <div>User Joined</div>;

4178b60588ef1cf382f85120e203ed67561ae854
Sachin Alam <sachinalam4216@gmail.com>
2025-01-13 11:16:44 +0530

    code editor setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Editor from "@monaco-editor/react";
 import { useState } from "react";
 import io from "socket.io-client";
 import "./App.css";
@@ -8,6 +9,8 @@ const App = () => {
   const [joined, setJoined] = useState(false);
   const [roomId, setRoomId] = useState("");
   const [username, setUserName] = useState("");
+  const [language, setLanguage] = useState("javascript");
+  const [code, setCode] = useState("");
 
   const joinRoom = () => {
     // console.log(roomId, username);
@@ -16,6 +19,12 @@ const App = () => {
       socket.emit("join", { roomId, username });
       setJoined(true);
     }
+  };
+
+  const copyRoomId = () => {};
+
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
   };
 
   if (!joined) {
@@ -44,7 +53,50 @@ const App = () => {
     );
   }
 
-  return <div>User Joined</div>;
+  return (
+    <div className="editor-container">
+      <div className="sidebar">
+        <div className="room-info">
+          <h2>Code Room: {roomId}</h2>
+          <button className="copy-button" onClick={copyRoomId}>
+            Copy ID
+          </button>
+        </div>
+
+        <h3>Users in Room</h3>
+        <ul>
+          <li>Sachin</li>
+          <li>Sehwag</li>
+        </ul>
+
+        <p className="typing-indicator">user typing...</p>
+
+        <select className="language-selector">
+          <option value="javascript">Javascript</option>
+          <option value="python">Python</option>
+          <option value="java">Java</option>
+          <option value="cpp">C++</option>
+        </select>
+
+        <button className="leave-button">Leave Room</button>
+      </div>
+
+      <div className="editor-wrapper">
+        <Editor
+          height={"100%"}
+          defaultLanguage={language}
+          language={language}
+          value={code}
+          onChange={handleCodeChange}
+          theme="vs-dark"
+          options={{
+            minimap: { enabled: false },
+            fontSize: 14,
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
